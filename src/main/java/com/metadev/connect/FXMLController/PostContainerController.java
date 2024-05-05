6e6cf44c6b5e9a195a439539769f8e30180d8fb8
ngyuheng2003 <ngyuheng2003@gmail.com>
2024-05-05 16:52:48 +0800

    Bug Fixed

--- src/main/java/com/metadev/connect/FXMLController/PostContainerController.java
+++ src/main/java/com/metadev/connect/FXMLController/PostContainerController.java
@@ -4,9 +4,13 @@ import com.metadev.connect.Entity.Post;
 import com.metadev.connect.Entity.User;
 import com.metadev.connect.Repository.UserRepository;
 import com.metadev.connect.Service.UserService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import java.util.Objects;
@@ -14,6 +18,10 @@ import java.util.Objects;
 
 
 public class PostContainerController {
+    @FXML private Button postMenuButton;
+
+    @FXML private VBox postMenuContainer;
+
     @FXML private Text post_content;
 
     @FXML private Text post_createdDate;
@@ -31,4 +39,20 @@ public class PostContainerController {
         this.post_content.setText(post.getContent());
     }
 
+    public void postMenuButtonClicked(ActionEvent event) {
+        if(postMenuContainer.getOpacity() == 0){
+            postMenuContainer.setOpacity(1);
+            postMenuContainer.setDisable(false);
+        }else{
+            postMenuContainer.setOpacity(0);
+            postMenuContainer.setDisable(true);
+        }
+    }
+
+    public void containerMouseClicked(MouseEvent mouseEvent) {
+        if(postMenuContainer.getOpacity() == 1){
+            postMenuContainer.setOpacity(0);
+            postMenuContainer.setDisable(true);
+        }
+    }
 }
